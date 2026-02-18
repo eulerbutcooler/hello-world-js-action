657441d11f9476eacac681a50d97482efec72363
eulerbutcooler <eulerbutcooler@gmail.com>
2026-02-18 19:29:39 +0530

    initial version

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import * as core from "@actions/core";
 
-const name = core.getInput("name");
-const outputval = `Ji boliye ${name}`;
-core.setOutput("greeting", outputval);
+try {
+  const name = core.getInput("name");
+  const outputval = `Ji boliye ${name}`;
+  core.setOutput("greeting", outputval);
+} catch (error) {
+  core.setFailed(error.message);
+}
